1895b79fbbab078cda0d73339f6dd4d9a31faaee
SadaqathUmair-SU <63862344+SadaqathUmair-SU@users.noreply.github.com>
2022-03-24 00:02:00 +0530

    Update sample-script.js

--- horoscopeNFTSolidity/scripts/sample-script.js
+++ horoscopeNFTSolidity/scripts/sample-script.js
@@ -26,8 +26,7 @@ async function main() {
   await txn.wait();
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
